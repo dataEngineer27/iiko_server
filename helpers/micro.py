ebd81d5e7c41f89bd414b53185cf4e2830cd6c08
Data Engineer <dataeng27@gmail.com>
2023-11-07 14:41:37 +0500

    update project with time status

--- helpers/micro.py
+++ helpers/micro.py
@@ -73,7 +73,7 @@ def shift_payments(key, session_id):
 
 
 def department_revenue(key, department):
-    department_data = requests.get(f"{BASE_URL}/resto/api/reports/sales?key={key}&department={department}&dateFrom=01.01.2023&dateTo=30.09.2023&dishDetails=true&allRevenue=true")
+    department_data = requests.get(f"{BASE_URL}/resto/api/reports/sales?key={key}&department={department}&dateFrom=01.01.2022&dateTo=31.12.2022&dishDetails=true&allRevenue=true")
     root = ET.fromstring(department_data.content)
     corporate_item_dtos = root.findall('dayDishValue')
     return corporate_item_dtos
