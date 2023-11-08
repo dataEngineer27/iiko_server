899989d75936f00223d1032c7f15e44e753d2ab2
Data Engineer <dataeng27@gmail.com>
2023-11-08 10:25:13 +0500

    update project with time status

--- main.py
+++ main.py
@@ -98,10 +98,10 @@ def job_stopper():
 # schedule.every().tuesday.at("12:48").do(departments_starter)  # 1
 # schedule.every().day.at("08:00").do(nomenclature_categories_starter)  # 2
 # schedule.every().day.at("13:05").do(nomenclatures_starter)  # 4
-schedule.every().day.at("15:03").do(department_revenue_starter)   # 5
+schedule.every().day.at("10:30").do(department_revenue_starter)   # 5
 
 # Stop all jobs
-schedule.every().day.at('18:00').do(job_stopper)
+schedule.every().day.at('16:00').do(job_stopper)
 
 while True:
     time.sleep(20)
