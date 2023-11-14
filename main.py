7a18ca1d31a02b4a41be7a1aed28ded4d0e58234
Data Engineer <dataeng27@gmail.com>
2023-11-14 14:34:32 +0500

    update payments function

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ from jobs import *
 # schedule.every().day.at("18:16").do(employee_roles_starter)   # 6
 # schedule.every().day.at("18:16").do(employees_starter)   # 7
 # schedule.every().day.at("10:50").do(shift_list_starter)   # 8
-schedule.every().day.at("02:50").do(payments_starter)   # 9
+schedule.every().day.at("14:33").do(payments_starter)   # 9
 # schedule.every().day.at("18:16").do(product_expense_starter)   # 10
 
 # Stop all jobs
-schedule.every().day.at('06:30').do(job_stopper)
+schedule.every().day.at('18:35').do(job_stopper)
 
 while True:
     time.sleep(20)
